a449781613da177a22ea82042884a9d31d8c3884
Rob Cabrera <rscabrera@gmail.com>
2022-08-18 12:45:24 -0500

    added time

--- break_time.py
+++ break_time.py
@@ -4,6 +4,7 @@ import time
 total_breaks = 3
 break_count = 0
 
+print("This program started on " +time.ctime())
 while(break_count < total_breaks):
     time.sleep(10)
     webbrowser.open("https://youtube.com/watch?v=dQw4w9WgXcQ")
